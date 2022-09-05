516962015304618ba55bb6e7061d53d02bdf4089
Willzi3 <williamszachary787@gmail.com>
2022-09-05 09:42:52 +0200

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const cartRoute = require('./routes/cartRoute.js')
 
 app.use("/users", userRoute)
 app.use("/products", productRoute)
-app.use("/cart", cartRoute)
+app.use(cartRoute)
 
 app.listen(app.get("port"), () => {
     console.log(`Listening for calls on port ${app.get("port")}`);
